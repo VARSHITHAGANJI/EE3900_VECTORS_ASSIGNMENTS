c9733b1a7e6cf30a3287aedc0475fc2f5f7c1d26
VARSHITHAGANJI <80575682+VARSHITHAGANJI@users.noreply.github.com>
2021-08-28 21:03:17 +0530

    Update skew_lines_code.py

--- LINEAR_FORMS_ASSIGNMENT4/skew_lines_code.py
+++ LINEAR_FORMS_ASSIGNMENT4/skew_lines_code.py
@@ -6,7 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
-
 ## Line 1 = (1,2,3) + lambda1(1,-3,2)
 ## Line 2 = (4,5,6) + lambda2(2,3,1)
 
@@ -63,7 +62,7 @@ B2 = np.array([2,3,1]).reshape((3,1))
 #Generating line 1
 x_AB = line_gen(A2,B2)
 #plotting line
-plt.plot(x_AB[0,:],x_AB[1,:],x_AB[2,:],color='R',label='Line 2')
+plt.plot(x_AB[0,:],x_AB[1,:],x_AB[2,:],color='r',label='Line 2')
  
 
 
@@ -75,12 +74,17 @@ ax.text(4 * (1 + 0.1), 5* (1 - 0.1) ,6*(1-0.1), "(4,5,6)")
 
 ##Finding shortest distance between skew lines
 B_array=np.concatenate((B2,B1),axis=1)
-A=np.dot(B_array.T,B_array)
+A=B_array.T@B_array
 
-B=np.dot(B_array.T,(A1-A2))
+B=B_array.T@(A1-A2)
 #Solving for lamba values using the equation Ax=B
 lambda_v = np.linalg.solve(A,B)
 # lamba_1 and -lambda_2 are the entries of above matrix
+print('(lambda1,-lambda2)(calculated)=',lambda_v)
+
+#Verifying with built in least square function
+Y= np.linalg.lstsq(A,B)
+print('(lambda1,-lambda2)(inbuilt)=',Y[0])
 
 #positional vectors of end points of shortest distance line 
 p1=point_gen(A1,B1,lambda_v[0,0])
@@ -91,8 +95,11 @@ d=norm(p1,p2)
 
 print('The shortest distance between skew lines is: ',d)
 
+
 #plotting shortest line
 p=np.concatenate((p1,p2),axis=1)
+
+
 
 plt.plot(p[0,:],p[1,:],p[2,:],color='b',label='shortest line')
 
@@ -103,6 +110,10 @@ ax.text(-0.4736 * (1 + 0.1), 6.4210* (1 - 0.1) ,0.0526*(1-0.1),"p1")
 
 ax.scatter(5.05263, 6.57894, 6.5263,'y',label='p2')
 ax.text(5.05263 * (1 + 0.1), 6.57894* (1 - 0.1) ,6.5263*(1-0.1),"p2")
+
+
+
+
 
 plt.xlabel('$x$');plt.ylabel('$y$')
 plt.grid()
